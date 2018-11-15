d32789ca5b76b2e2dd11303a2c5f47a3c428d993
Michael Stein <m.stein@h3-ms.com>
2018-11-15 10:33:48 +0100

    Adds Windows build steps.

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools, Meson
 import os
 
 
 class GLibConan(ConanFile):
     name = "glib"
-    version = "2.57.1"
+    version = "2.58.1"
     description = "GLib provides the core application building blocks for libraries and applications written in C"
     url = "https://github.com/bincrafters/conan-glib"
     homepage = "https://github.com/GNOME/glib"
@@ -19,15 +19,25 @@ class GLibConan(ConanFile):
     default_options = "shared=False", "fPIC=True", "with_pcre=False"
     source_subfolder = "source_subfolder"
     autotools = None
+    generators = "pkg_config"
 
     def configure(self):
-        if self.settings.os != 'Linux':
-            raise Exception("GNOME glib is only supported on Linux for now.")
+        if self.settings.os == "Windows":
+            if self.settings.compiler != "Visual Studio":
+                raise Exception("Only Visual Studio is supported on Windows for now.")
+            del self.options.fPIC
         del self.settings.compiler.libcxx
+
+    def build_requirements(self):
+        if self.settings.os == "Windows":
+            self.build_requires("ninja_installer/1.8.2@bincrafters/stable")
 
     def requirements(self):
         if self.options.with_pcre:
-            self.requires.add("pcre/8.41@bincraftres/stable")
+            self.requires("pcre/8.41@bincraftres/stable")
+        if self.settings.os == "Windows":
+            self.requires("libffi/3.2.1@bincrafters/stable")
+            self.requires("zlib/1.2.11@conan/stable")
 
     def source(self):
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version))
@@ -58,19 +68,39 @@ class GLibConan(ConanFile):
         return self.autotools
 
     def build(self):
-        autotools = self._configure_autotools()
-        with tools.chdir(self.source_subfolder):
-            autotools.make()
+        if self.settings.os == 'Windows':
+            meson = Meson(self)
+            meson.configure(
+                source_folder=os.path.join(self.build_folder, self.source_subfolder),
+                pkg_config_paths=[self.build_folder]
+            )
+            meson.build()
+        else:
+            autotools = self._configure_autotools()
+            with tools.chdir(self.source_subfolder):
+                autotools.make()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self.source_subfolder)
-        autotools = self._configure_autotools()
-        with tools.chdir(self.source_subfolder):
-            autotools.make(["install"])
+        if self.settings.os == 'Windows':
+            self.copy("glibconfig.h", dst="include", src="glib")
+            self.copy("*.h", dst="include/glib", src=self.source_subfolder + "/glib")
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
+            self.copy("*.so", dst="lib", keep_path=False)
+            self.copy("*.dylib", dst="lib", keep_path=False)
+            self.copy("*.a", dst="lib", keep_path=False)
+        else:
+            autotools = self._configure_autotools()
+            with tools.chdir(self.source_subfolder):
+                autotools.make(["install"])
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
+            self.cpp_info.libs = tools.collect_libs(self)
             self.cpp_info.libs.append("pthread")
-        self.cpp_info.includedirs.append(os.path.join('include', 'glib-2.0'))
-        self.cpp_info.includedirs.append(os.path.join('lib', 'glib-2.0', 'include'))
+            self.cpp_info.includedirs.append(os.path.join('include', 'glib-2.0'))
+            self.cpp_info.includedirs.append(os.path.join('lib', 'glib-2.0', 'include'))
+        elif self.settings.os == "Windows":
+            self.cpp_info.libs = ["gio-2.0", "glib-2.0", "gmodule-2.0", "gobject-2.0", "gthread-2.0", "intl"]
+            self.cpp_info.includedirs.append(os.path.join('include', 'glib'))
